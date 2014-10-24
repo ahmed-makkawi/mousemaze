528650af006c253fd6ac445cefb700f7ca69c82d
Mohamed Ahmed <mohamed.galil@outlook.com>
2014-10-24 23:04:17 +0200

    Fixed Block.java Removed constructor from Constants.java

--- src/maze/utilities/Constants.java
+++ src/maze/utilities/Constants.java
@@ -9,16 +9,13 @@ package maze.utilities;
  */
 public class Constants {
 
-    /**
+	/**
      * 
      */
 
-    public static final int MOUSE 	= 0;
-    public static final int HORIZONTAL 	= 1;
-    public static final int VERTICAL	= 2;
-
-    public Constants() {
-	// TODO Auto-generated constructor stub
-    }
+	public static final int MOUSE = 0;
+	public static final int HORIZONTAL = 1;
+	public static final int VERTICAL = 2;
+	public static final int GAP = -1;
 
 }
